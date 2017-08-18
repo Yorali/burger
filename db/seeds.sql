048a43d9174bfa9fd2cbc6775e76368ae8a71b7e
Yorali <hideayeni@gmail.com>
2017-08-18 14:39:17 -0500

    Finally managed to gain cooperation of mysql... For now

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name,devoured,date_eaten) VALUES ('Cygnus','0','2017-8-15');
+INSERT INTO burgers (burger_name,devoured,date_eaten) VALUES ('Crying Star','0','2017-8-15');
+INSERT INTO burgers (burger_name,devoured,date_eaten) VALUES ('Deux','0','2017-8-15');
