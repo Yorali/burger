103a921be1911099b77b5ab738310039bf8b322d
Yorali <hideayeni@gmail.com>
2017-08-20 20:07:46 -0500

    Don't know how to fix error thrown by express module

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burgers_controller.js");
 
 var app = express();
+
+var port = process.env.PORT || 4000;
+
+app.use("/static", express.static('public'));
+app.use(bodyParser.urlencoded({ extended:false }));
+
+app.use(methodOverride("_method"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use("/", routes);
+
+app.listen(port, function() {
+    console.log("Connected at port: " + port);
+});
